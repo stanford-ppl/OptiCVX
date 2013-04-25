875a4b35929780439890d375affc5f6082dd3ebe
Christopher De Sa <cdesa@stanford.edu>
2013-04-24 17:50:40 -0700

    Tried a different homogeneous solver.

--- src/main/scala/ppl/dsl/opticvx/solvers/PrimalDualHomogeneousEx.scala
+++ src/main/scala/ppl/dsl/opticvx/solvers/PrimalDualHomogeneousEx.scala
@@ -11,24 +11,41 @@ object PrimalDualHomogeneousEx extends SolverGenHomogeneous with SolverGenUtil {
 
   def hcode(A: Almap, K: Cone, tol: AVector): AVector = {
     val x = vector(A.domain)
-    val z = vector(A.domain)
+    x := K.central_vector(A.input)
+
     val u = vector(A.domain)
     val cond = scalar
 
-    val Aproj = new LSQRProject(A, zeros(A.codomain))
+    val nms = 10
 
-    x := K.central_vector(A.input)
-    u := zeros(u.size)
+    val z = for(i <- 0 until nms) yield vector(A.domain)
+    for(i <- 0 until nms) z(i) := zeros(A.domain)
 
     converge(cond - tol) {
-      z := Aproj.proj(x - u, tol)
-      x := K.project(z + u)
-      u := u + z - x
+      // recreate the vectors
+      z(0) := A.T * A * x
+      for(i <- 1 until nms) {
+        z(0) := z(0) - z(i) * dot(z(0),z(i))
+      }
+      z(0) := z(0) / sqrt(norm2(z(0)))
+      // perform the projection
+      converge(cond - tol) {
+        for(i <- 1 until nms) {
+          x := x - z(i) * dot(x, z(i))
+        }
+        u := K.project(x)
+        cond := sqrt(norm2(u - x))
+        x := u
+      }
+      // update the vectors
+      for(i <- 1 until nms) {
+        z(i) := z(i - 1)
+      }
       cond := norm_inf(A*x)
     }
 
     return x
   }
-  
+
 }
 
