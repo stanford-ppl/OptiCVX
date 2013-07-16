cffb3bf47716a646fad84896b77e8a48b00993c7
Christopher De Sa <cdesa@stanford.edu>
2013-07-16 16:16:10 -0700

    Added beta substitution.

--- src/main/scala/ppl/dsl/opticvx/la/TypeChecker.scala
+++ src/main/scala/ppl/dsl/opticvx/la/TypeChecker.scala
@@ -20,4 +20,89 @@ object TypeChecker {
     case EIApp(fx, arg) => TIApp(typeOf(fx, env), arg)
     case ep: EPrimitive => ep.ltype
   }
+
+  def betaSub(x: LExpr): LExpr = x match {
+    case EIApp(EILambda(i, b), n) => isub(b, Map(i -> n))
+    case ELambda(t, id, body) => {
+      val u = betaSub(body)
+      if(u == x) x else ELambda(t, id, u)
+    }
+    case EILambda(id, body) => {
+      val u = betaSub(body)
+      if(u == x) x else EILambda(id, u)
+    }
+    case EApp(f, a) => {
+      val uf = betaSub(f)
+      val ua = betaSub(a)
+      if((uf == f)&&(ua == a)) x else EApp(uf, ua)
+    }
+    case EIApp(f, a) => {
+      val uf = betaSub(f)
+      if(uf == f) x else EIApp(uf, a)
+    }
+    case _ => x
+  }
+
+  def sub(x: LExpr, s: Map[Int, LExpr]): LExpr = x match {
+    case EVar(id) => s.getOrElse(id, x)
+    case ELambda(t, id, body) => {
+      val u = sub(body, s - id)
+      if(u == x) x else ELambda(t, id, u)
+    }
+    case EILambda(id, body) => {
+      val u = sub(body, s)
+      if(u == x) x else EILambda(id, u)
+    }
+    case EApp(f, a) => {
+      val uf = sub(f, s)
+      val ua = sub(a, s)
+      if((uf == f)&&(ua == a)) x else EApp(uf, ua)
+    }
+    case EIApp(f, a) => {
+      val uf = sub(f, s)
+      if(uf == f) x else EIApp(uf, a)
+    }
+    case _ => x
+  }
+
+  def isub(x: LExpr, s: Map[Int, LIExpr]): LExpr = x match {
+    case ELambda(t, id, body) => {
+      val u = isub(body, s)
+      if(u == x) x else ELambda(t, id, u)
+    }
+    case EILambda(id, body) => {
+      val u = isub(body, s - id)
+      if(u == x) x else EILambda(id, u)
+    }
+    case EApp(f, a) => {
+      val uf = isub(f, s)
+      val ua = isub(a, s)
+      if((uf == f)&&(ua == a)) x else EApp(uf, ua)
+    }
+    case EIApp(f, a) => {
+      val uf = isub(f, s)
+      val ua = isub(a, s)
+      if((uf == f)&&(ua == a)) x else EIApp(uf, ua)
+    }
+    case _ => x
+  }
+
+  def isub(x: LIExpr, s: Map[Int, LIExpr]): LIExpr = x match {
+    case IVar(id) => s.getOrElse(id, x)
+    case IAdd(a, b) =>  {
+      val ua = isub(a, s)
+      val ub = isub(b, s)
+      if((ua == a)&&(ub == b)) x else IAdd(ua, ub)
+    }
+    case IMult(a, b) => {
+      val ua = isub(a, s)
+      val ub = isub(b, s)
+      if((ua == a)&&(ub == b)) x else IMult(ua, ub)
+    }
+    case ISxp(a, e) => {
+      val ua = isub(a, s)
+      if(ua == a) x else ISxp(ua, e)
+    }
+    case _ => x
+  }
 }
